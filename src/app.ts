eb6fd8f01100f4c00c9929c5c06fdbed0d1d8995
NusratParvin <nusrat.official@gmail.com>
2024-07-12 19:21:26 +0400

    timestamp

--- src/app.ts
+++ src/app.ts
@@ -11,7 +11,7 @@ app.use(cors())
 app.use('/api/', router)
 
 app.get('/', (req: Request, res: Response) => {
-  res.send('Welcome to Bike Rental Service!')
+  res.send('strideX on!')
 })
 
 app.use(globalErrorHandler)
--- src/app/modules/products/products.model.ts
+++ src/app/modules/products/products.model.ts
@@ -1,15 +1,18 @@
 import { Schema, model } from 'mongoose'
 import { TProduct } from './products.interface'
 
-const productSchema = new Schema<TProduct>({
-  name: { type: String, required: true },
-  category: { type: String, required: true },
-  stockQuantity: { type: Number, required: true },
-  brand: { type: String, required: true },
-  rating: { type: Number, required: true },
-  description: { type: String, required: true },
-  price: { type: Number, required: true },
-  image: { type: String, required: true },
-})
+const productSchema = new Schema<TProduct>(
+  {
+    name: { type: String, required: true },
+    category: { type: String, required: true },
+    stockQuantity: { type: Number, required: true },
+    brand: { type: String, required: true },
+    rating: { type: Number, required: true },
+    description: { type: String, required: true },
+    price: { type: Number, required: true },
+    image: { type: String, required: true },
+  },
+  { timestamps: true },
+)
 
 export const Product = model<TProduct>('Product', productSchema)
